1f8a746ec8af5e8f27130ed9607403e4e8b505c9
eloy98g <eloygomez132@gmail.com>
2021-06-26 12:43:44 +0200

    imagepicker done

--- src/components/new/ImagePicker.jsx
+++ src/components/new/ImagePicker.jsx
@@ -1,44 +1,44 @@
-import React from "react";
-import {  Text, Flex } from "@chakra-ui/react";
+// import React from "react";
+// import {  Text, Flex } from "@chakra-ui/react";
 
-// File
-import FileInput from "react-simple-file-input";
+// // File
+// import FileInput from "react-simple-file-input";
 
-const ImagePicker = ({ title, optional, placeholder, onChange, ...rest }) => {
-	const allowedFileTypes = ["image/png", "image/jpeg", "image/gif"];
+// const ImagePicker = ({ title, optional, placeholder, onChange, ...rest }) => {
+// 	const allowedFileTypes = ["image/png", "image/jpeg", "image/gif"];
 
-	const fileIsIncorrectFiletype = (file) => {
-		if (allowedFileTypes.indexOf(file.type) === -1) {
-			return true;
-		} else {
-			return false;
-		}
-	};
-	return (
-		<Flex flexDirection="column" mb={4} {...rest}>
-			<Text fontWeight={"bold"} lineHeight={2}>
-				{title}
-				{optional ? "" : " *"}
-			</Text>
-			<FileInput
-				style={{
-					width: "100%",
-					height: "100%",
-				}}
-				readAs="binary"
-				onLoad={(event) => onChange(btoa(event.target.result))}
-				cancelIf={fileIsIncorrectFiletype}
-			/>
-		</Flex>
-	);
-};
+// 	const fileIsIncorrectFiletype = (file) => {
+// 		if (allowedFileTypes.indexOf(file.type) === -1) {
+// 			return true;
+// 		} else {
+// 			return false;
+// 		}
+// 	};
+// 	return (
+// 		<Flex flexDirection="column" mb={4} {...rest}>
+// 			<Text fontWeight={"bold"} lineHeight={2}>
+// 				{title}
+// 				{optional ? "" : " *"}
+// 			</Text>
+// 			{/* <FileInput
+// 				style={{
+// 					width: "100%",
+// 					height: "100%",
+// 				}}
+// 				readAs="binary"
+// 				onLoad={(event) => onChange(btoa(event.target.result))}
+// 				cancelIf={fileIsIncorrectFiletype}
+// 			/> */}
+// 		</Flex>
+// 	);
+// };
 
-export default ImagePicker;
+// export default ImagePicker;
 
 // Styled image picker (wip)
-/*
+
 import React, { useState } from "react";
-import { Text, Flex } from "@chakra-ui/react";
+import { Text, Flex, Image } from "@chakra-ui/react";
 
 // File
 import Files from "react-files";
@@ -47,16 +47,29 @@ import Files from "react-files";
 import { MdFileUpload } from "react-icons/md";
 
 const ImagePicker = ({ title, optional, placeholder, onChange, ...rest }) => {
-	const [setImage] = useState(placeholder);
-	const allowedFileTypes = ["image/png", "image/jpeg", "image/gif"];
+	const [image, setImage] = useState(placeholder);
 
-	const fileIsIncorrectFiletype = (file) => {
-		if (allowedFileTypes.indexOf(file.type) === -1) {
-			return true;
-		} else {
-			return false;
-		}
+	const toDataUrl = (url, callback) => {
+		var xhr = new XMLHttpRequest();
+		xhr.onload = function () {
+			var reader = new FileReader();
+			reader.onloadend = function () {
+				callback(reader.result);
+			};
+			reader.readAsDataURL(xhr.response);
+		};
+		xhr.open("GET", url);
+		xhr.responseType = "blob";
+		xhr.send();
 	};
+
+	const handleImage = (url) => {
+		toDataUrl(url, (myBase64) => {
+			onChange(myBase64);
+			setImage(url);
+		});
+	};
+
 	return (
 		<Flex flexDirection="column" mb={4}>
 			<Text fontWeight={"bold"} lineHeight={2}>
@@ -86,13 +99,23 @@ const ImagePicker = ({ title, optional, placeholder, onChange, ...rest }) => {
 						alignItems: "center",
 					}}
 					className="files-dropzone"
-					onChange={(e) => console.log("ee", e.target.file)}
+					onChange={(e) => handleImage(e[0].preview.url)}
 					accepts={["image/png", "image/jpeg", "image/gif"]}
 					maxFileSize={10000000}
 					minFileSize={0}
 					clickable
 				>
-					<MdFileUpload fontSize="56px" />
+					{image ? (
+						<Image
+							src={image}
+							alt="image"
+							w="100%"
+							h="100%"
+							objectFit="cover"
+						/>
+					) : (
+						<MdFileUpload fontSize="56px" />
+					)}
 				</Files>
 			</Flex>
 		</Flex>
@@ -100,5 +123,3 @@ const ImagePicker = ({ title, optional, placeholder, onChange, ...rest }) => {
 };
 
 export default ImagePicker;
-
-*/
